958c9481b47eb15096204270ac08f67b79cb3d1c
Kubaikin Dmitry <dimian150@gmail.com>
2020-11-19 15:49:25 +0200

    start

---
+++ Money/Money.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_H
+{
+    class Money:Exception
+    {
+        private double sum;
+        
+        public Money(double num)
+        {
+            try
+            {
+                if (num>=0)
+                {
+                    sum = Math.Round(num, 2);
+                }
+                else
+                {
+                    throw new Exception("Значение не может быть меньше 0");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+        }
+
+        public static Money operator +(Money n1, Money n2)
+        {
+            return new Money(n1.sum + n2.sum);
+        }
+        public static Money operator *(Money n1, Money n2)
+        {
+            return new Money(n1.sum * n2.sum);
+        }
+        public static Money operator -(Money n1, Money n2)
+        {
+            return new Money(n1.sum - n2.sum);
+        }
+        public static Money operator /(Money n1, Money n2)
+        {
+            return new Money(n1.sum / n2.sum);
+        }
+        public static Money operator ++(Money n1)
+        {
+            return new Money(n1.sum += 0.01);
+        }
+        public static Money operator --(Money n1)
+        {
+            return new Money(n1.sum -= 0.01);
+        }
+        public static bool operator <(Money n1, Money n2)
+        {
+            return n1.sum < n2.sum;
+        }
+        public static bool operator >(Money n1, Money n2)
+        {
+            return n1.sum > n2.sum;
+        }
+        public static bool operator ==(Money n1, Money n2)
+        {
+            return n1.sum == n2.sum;
+        }
+        public static bool operator !=(Money n1, Money n2)
+        {
+            return !(n1.sum == n2.sum);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return this.ToString()==obj.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"Сумма {sum} грн";
+        }
+    }
+}
---
+++ Money/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_H
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money(2);
+            Money money2 = new Money(45);
+
+            Money money3 = money + money2;
+            Console.WriteLine(money3.ToString());
+
+            Money money4 = money - money2;
+            Console.WriteLine(money4.ToString());
+
+            Money money5 = money * money2;
+            Console.WriteLine(money5.ToString());
+
+            Money money6 = money / money2;
+            Console.WriteLine(money6.ToString());
+
+            money3++;
+            Console.WriteLine(money3.ToString());
+
+            money5--;
+            Console.WriteLine(money5.ToString());
+
+            if (money>money2)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            if (money==money2)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
